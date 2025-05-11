d0607d4e14a28795c44a765b1698c60a0df46ce3
Dino0204 <s24020@gsm.hs.kr>
2025-05-12 02:27:23 +0900

    feat: room model

---
+++ server/models/room.model.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+// @Entity
+// @Repository
+const roomSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Room", roomSchema);
