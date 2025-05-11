731fc035c1622383552ec35d32e26b4170276f5d
Dino0204 <s24020@gsm.hs.kr>
2025-05-12 02:27:15 +0900

    feat: message model

---
+++ server/models/message.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    room: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Room",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
