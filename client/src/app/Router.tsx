8df66d065ea9901a329b114007046587f02cc2b0
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 00:49:30 +0900

    feat: cookie provider

--- client/src/app/Router.tsx
+++ client/src/app/Router.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Main from "../views/main/ui"
 import Signin from "../views/signin/ui"
 import Chat from "../views/Chat/ui"
+import { CookiesProvider } from "react-cookie"
 
 function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   )
 }
 
