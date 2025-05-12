6bfbe0b4d79488dcee4aa63cf8f87f6338424502
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 00:49:19 +0900

    feat: cookie parser

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/auth.route.js");
 const roomRoutes = require("./routes/room.route");
 
 const app = express();
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/rooms", roomRoutes);
 
 app.get("/", (req, res) => {
+  console.log("Cookies: ", req.cookies);
+
   res.send("Dinocord API is running");
 });
 
