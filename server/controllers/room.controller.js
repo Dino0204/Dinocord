0e2e595e06a7aec15ba774edd6efc57c3aa5a1d8
Dino0204 <s24020@gsm.hs.kr>
2025-05-12 17:17:01 +0900

    🐛 fix: room controller json

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -3,33 +3,37 @@ const Room = require("../models/room.model");
 
 // @RestController
 exports.create = async (req, res) => {
-  const { roomName, ownerName } = req.body;
+  try {
+    const { roomName, ownerName } = req.body;
 
-  if ((!roomName, !ownerName)) {
-    return res.status(400).json({ error: "roomName, ownerName is required" });
+    if ((!roomName, !ownerName)) {
+      return res.status(400).json({ error: "roomName, ownerName is required" });
+    }
+
+    const owner = await User.findOne({ name: ownerName });
+
+    if (!owner) {
+      return res
+        .status(404)
+        .json({ error: `This user(${ownerName}) does not exist` });
+    }
+
+    let room = await Room.findOne({ name: roomName });
+
+    if (room) {
+      return res
+        .status(409)
+        .json({ error: `This room(${roomName}) already exist` });
+    }
+
+    room = await Room.create({
+      name: roomName,
+      members: [owner],
+      owner: owner,
+    });
+
+    return res.status(200).json({ room });
+  } catch (error) {
+    return res.json({ message: error.message });
   }
-
-  const owner = await User.findOne({ name: ownerName });
-
-  if (!owner) {
-    return res
-      .status(404)
-      .json({ error: `This user(${ownerName}) does not exist` });
-  }
-
-  const room = await Room.findOne({ name: roomName });
-
-  if (room) {
-    return res
-      .status(409)
-      .json({ error: `This room(${roomName}) already exist` });
-  }
-
-  room = await Room.create({
-    name: roomName,
-    members: [owner],
-    owner: owner,
-  });
-
-  res.status(200).json({ room });
 };
