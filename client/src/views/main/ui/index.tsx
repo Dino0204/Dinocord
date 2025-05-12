1df50fb582535c56bad15f6ea7b4e68570b068cb
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 00:50:20 +0900

    🎨 improve: main

--- client/src/views/main/ui/index.tsx
+++ client/src/views/main/ui/index.tsx
@@ -1,7 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const Main = () => {
+  const router = useNavigate()
+
   return (
-    <main className="w-md h-screen mx-auto grid grid-cols-1 grid-row-12 text-white">
-      Dinocord
+    <main className="w-md h-screen mx-auto grid grid-cols-1 grid-row-12 bg-gray-700">
+      <header className="flex justify-between p-2 w-full h-max text-white bg-gray-900">
+        <h1>Dinocord</h1>
+        <button className="cursor-pointer" onClick={() => router("/signin")}>Signin</button>
+      </header>
     </main>
   )
 }
