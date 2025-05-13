986520cd0e28a30a86093d09d02d1843c18ab2a4
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 17:02:46 +0900

    fix: room create

--- server/controllers/room.controller.js
+++ server/controllers/room.controller.js
@@ -4,18 +4,16 @@ const Room = require("../models/room.model");
 // @RestController
 exports.create = async (req, res) => {
   try {
-    const { roomName, ownerName } = req.body;
+    const { roomName } = req.body;
+    const { email } = req.user;
+    const owner = await User.findOne({ email });
 
-    if ((!roomName, !ownerName)) {
-      return res.status(400).json({ error: "roomName, ownerName is required" });
+    if (!roomName) {
+      return res.status(400).json({ error: "roomName is required" });
     }
 
-    const owner = await User.findOne({ name: ownerName });
-
     if (!owner) {
-      return res
-        .status(404)
-        .json({ error: `This user(${ownerName}) does not exist` });
+      return res.status(404).json({ error: `This user does not exist` });
     }
 
     let room = await Room.findOne({ name: roomName });
@@ -35,5 +33,34 @@ exports.create = async (req, res) => {
     return res.status(200).json({ room });
   } catch (error) {
     return res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getRooms = async (req, res) => {
+  try {
+    const { email } = req.user;
+    const currentUser = await User.findOne({ email });
+
+    if (!currentUser) {
+      return res.status(404).json({
+        ok: false,
+        message: "사용자를 찾을 수 없습니다",
+      });
+    }
+
+    const rooms = await Room.find({ members: currentUser._id }).populate(
+      "owner",
+      "name email"
+    );
+
+    return res.status(200).json({
+      ok: true,
+      rooms,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      message: error.message,
+    });
   }
 };
