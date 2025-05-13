412f3df8078307843dac9400bb16a6a4d20e43ca
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 14:39:44 +0900

    improve: main

--- client/src/views/main/ui/index.tsx
+++ client/src/views/main/ui/index.tsx
@@ -4,7 +4,7 @@ const Main = () => {
   const router = useNavigate()
 
   return (
-    <main className="w-md h-screen mx-auto grid grid-cols-1 grid-row-12 bg-gray-700">
+    <main className="w-full h-screen mx-auto grid grid-cols-1 grid-row-12 bg-gray-700">
       <header className="flex justify-between p-2 w-full h-max text-white bg-gray-900">
         <h1>Dinocord</h1>
         <button className="cursor-pointer" onClick={() => router("/signin")}>Signin</button>
