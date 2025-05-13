77933de39eec2a93e6c30a7ff19f80cf896eee75
Dino0204 <s24020@gsm.hs.kr>
2025-05-13 17:02:51 +0900

    fix: server setting

--- server/middleware/authJWT.js
+++ server/middleware/authJWT.js
@@ -11,7 +11,7 @@ const authJWT = (req, res, next) => {
 
     // token이 검증되었으면 req에 값을 세팅하고, 다음 콜백함수로 갑니다.
     if (result.ok) {
-      req.user = { email: result.email };
+      req.user = { name: result.name, email: result.email };
       next();
     }
     // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
--- server/routes/room.route.js
+++ server/routes/room.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const authJWT = require("../middleware/authJWT");
-const { create } = require("../controllers/room.controller");
+const { create, getRooms } = require("../controllers/room.controller");
 
 // @RequestMapping
 router.post("/create", authJWT, create);
+router.get("/current", authJWT, getRooms);
 
 module.exports = router;
