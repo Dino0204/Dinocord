d3d663cf884f7b8485c29f110f420056c203bb71
Dino0204 <s24020@gsm.hs.kr>
2025-05-20 10:02:35 +0900

    🔥 remove: auth middleware

--- server/routes/auth.route.js
+++ server/routes/auth.route.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const authJWT = require("../middleware/authJWT");
 const { signin, signup } = require("../controllers/auth.controller");
 
 // @RequestMapping
